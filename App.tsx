8258d6ab82a76a06ff7ba17e6d8a150dfd384ca3
Marcus Miguel <marcusmigueell@gmail.com>
2024-07-14 19:08:40 -0300

    criando os estilos iniciais do app

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { StatusBar } from "react-native";
+
+import { ThemeProvider } from "styled-components/native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
+
+import theme from "./src/theme";
 
 export default function App() {
+
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
     return (
-        <Groups />
+        <ThemeProvider theme={theme}>
+            <StatusBar 
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            { fontsLoaded ? <Groups /> : <Loading /> }
+        </ThemeProvider>
     )
 }
