4f5e1ee67010cced78be70684577184d776c891f
Marcus Miguel <marcusmigueell@gmail.com>
2024-07-14 22:20:13 -0300

    criando componente dinamico para button

--- src/components/ListEmpty/styles.ts
+++ src/components/ListEmpty/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 export const Container = styled.View`
     flex: 1;
     justify-content: center;
-    align-items: center
+    align-items: center;
 `;
 
 export const Message = styled.Text`
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
 
@@ -28,6 +29,10 @@ export function Groups() {
                 )}
                 contentContainerStyle={groups.length === 0 && { flex: 1 }}
                 ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+            />
+
+            <Button 
+                title="Criar nova turma"
             />
         </Container>
     )
