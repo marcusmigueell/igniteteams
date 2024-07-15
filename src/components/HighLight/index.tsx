a5faa9b7ab1bb30854d340a5a98799bf0bc7aabd
Marcus Miguel <marcusmigueell@gmail.com>
2024-07-14 22:03:43 -0300

    tela de turmas

---
+++ src/components/HighLight/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, SubTitle, Title } from "./styles";
+
+type Props = {
+    title: string
+    subtitle: string
+}
+
+export function HighLight({ title, subtitle }: Props) {
+    return (
+        <Container>
+            <Title>
+                {title}
+            </Title>
+
+            <SubTitle>
+                {subtitle}
+            </SubTitle>
+        </Container>
+    )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+
+    const [groups, setGroups] = useState<string[]>([]);
+
     return (
         <Container>
             <Header />
+            <HighLight 
+                title="Turmas"
+                subtitle="Jogue com sua turma"
+            />
+
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard 
+                        title={item}
+                    />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+            />
         </Container>
     )
 }
