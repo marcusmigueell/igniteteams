3a226748c70fafe8a49ab03263395459acfdc363
Marcus Miguel <marcusmigueell@gmail.com>
2024-07-15 00:39:45 -0300

    voltando o inicio

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
-import { Players } from "@screens/Players";
+import { Groups } from "@screens/Groups";
 import { Loading } from "@components/Loading";
 
 import theme from "./src/theme";
@@ -20,7 +20,7 @@ export default function App() {
                 backgroundColor="transparent"
                 translucent
             />
-            { fontsLoaded ? <Players /> : <Loading /> }
+            { fontsLoaded ? <Groups /> : <Loading /> }
         </ThemeProvider>
     )
 }
