ef7a3a3b387437b88c27f406cbbaa08ec6149996
Marcus Miguel <marcusmigueell@gmail.com>
2024-07-15 22:31:28 -0300

    criação inicial das routes

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
-import { Groups } from "@screens/Groups";
+import { Routes } from "./src/routes";
 import { Loading } from "@components/Loading";
 
 import theme from "./src/theme";
@@ -20,7 +20,7 @@ export default function App() {
                 backgroundColor="transparent"
                 translucent
             />
-            { fontsLoaded ? <Groups /> : <Loading /> }
+            { fontsLoaded ? <Routes /> : <Loading /> }
         </ThemeProvider>
     )
 }
